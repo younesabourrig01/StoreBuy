91044eb08e82e622a849d97c666efc150b95196c
younes <younesabourrig1ya@gmail.com>
2026-05-07 14:45:29 +0100

    reset pr service to upgrade

--- backend/product-service/routes/resendRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { sendResults } = require("../controller/sendResultsController");
-
-router.post("/bulk", sendResults);
-
-module.exports = router;
