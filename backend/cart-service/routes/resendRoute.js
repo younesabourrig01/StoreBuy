be724da129b42806e5389db2ecfbce37aa8553b3
younes <younesabourrig1ya@gmail.com>
2026-05-11 00:25:00 +0100

    fix conflet

--- backend/cart-service/routes/resendRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { sendResults } = require("../controllers/sendResultsController");
-
-router.get("/", sendResults);
-
-module.exports = router;
--- backend/favorite-service/routes/favoritRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { addToFav, removeFav, getFav } = require("../controllers/favController");
-const router = express.Router();
-
-router.post("/", addToFav);
-router.get("/", getFav);
-router.delete("/:product_id", removeFav);
-
-module.exports = router;
